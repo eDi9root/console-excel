009a1f1add98f28e6b252870f6e54d204ccef82e
Junseok Oh <119016575+eDi9root@users.noreply.github.com>
2024-02-09 04:02:11 +0000

    ADD: makefile

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,14 @@
-
 #include "Table.h"
+#include "Txt.h"
+#include "Cell.h"
+#include "Vector.h"
+#include "Stack.h"
+#include "NumStack.h"
 
 
 int main() { 
     Consolexcel::Txt table(5, 5);
-    std::ofstream out("test.txt");
+    std::ostream out("test.txt");
 
     table.reg_cell(new Cell("Hello~", 0, 0, &table), 0, 0);
     table.reg_cell(new Cell("C++", 0, 1, &table), 0, 1);
