81ce2024e216ed2c3770db2de334155f8a078560
Junseok Oh <119016575+eDi9root@users.noreply.github.com>
2024-02-09 04:19:32 +0000

    EDIT: main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,16 @@
-
+#include "Txt.h"
 #include <fstream>
 #include <iostream>
 
 
 int main() { 
-    Consolexcel::Txt Table(5, 5);
+    Consolexcel::Txt table(5, 5);
     std::ofstream out("test.txt");
 
 //    Table.reg_cell(new Cell("Hello~", 0, 0, &Table), 0, 0);
 //    Table.reg_cell(new Cell("C++", 0, 1, &Table), 0, 1);
 
-    std::cout << std:: endl << Table;
-    out << Table;
+//     std::cout << std:: endl << table;
+//    out << table;
     
 }
